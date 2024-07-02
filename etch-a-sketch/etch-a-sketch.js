c9b5cf0a467094811c09eee728149de4d534d2de
hliu <hlzh00@icloud.com>
2024-07-02 12:07:36 -0700

    Cleaned up work directory. Added page for etch-a-sketch

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,13 +1,13 @@
 const container = document.querySelector(".container");
 const square = document.createElement("div");
-
 square.id = "square";
 
 let pad_size = 20;
 
 function promptSize () {
-    let new_size = Number(prompt("How big do you want the sketchpad to be?"));
+    let new_size = parseInt(prompt("How big do you want the sketchpad to be?"));
     pad_size = new_size;
+    clearPad();
     createPad();
 }
 
@@ -27,6 +27,38 @@ function createPad () {
     
         container.appendChild(row);
     }
+
+    let cells = document.querySelectorAll(".cell");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("pointerenter", (event) => paint(event));
+    }
+}
+
+function clearPad() {
+    let rows = document.querySelectorAll(".row");
+    rows.forEach((row) => row.remove());
+}
+
+function paint(event) {
+    
+    if (event.buttons === 1) {
+        let r = 0;
+        let g = 0;
+        let b = 0;
+            
+        // Update the background color of the cell
+        event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        event.target.style.opacity = "1";
+    }
 }
 
 createPad();
+
+let clear = document.getElementById("clear");
+clear.addEventListener("click", function () {
+    clearPad();
+    createPad();
+});
+
+let resize = document.getElementById("resize");
+resize.addEventListener("click", promptSize);
